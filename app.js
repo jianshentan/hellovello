c666ccb013c68c53015685d891c6643885a09e65
jianshentan <jianshentan@gmail.com>
2014-12-10 02:54:45 -0500

    favicon error fix

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname,'public','media','favicon.ico'));
+app.use(favicon(path.join(__dirname,'public','media','favicon.ico')));
 
 app.use('/', routes);
 
