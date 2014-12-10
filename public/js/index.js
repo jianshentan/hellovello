08d9c6fda95214b0bd6af71ea56123c4f370683a
jianshentan <jianshentan@gmail.com>
2014-12-10 01:30:59 -0500

    first

---
+++ public/js/index.js
@@ -0,0 +1,227 @@
+var genecoinPress = { ibtimes: { title: "International Business Times", link: "http://www.ibtimes.co.uk/genecoin-backup-your-dna-using-bitcoins-1472913" },
+                      huffingtonpost : { title: "Huffington Post", link: "http://www.huffingtonpost.co.uk/2014/10/30/genecoin-genome-backup-bitcoin_n_6076366.html" },
+                      animalny: { title: "Animal New York", link: "http://animalnewyork.com/2014/mysterious-entity-named-genecoin-wants-backup-dna-bitcoin/" },
+                      cryptocoin: { title: "Cryptocoin News", link: "https://www.cryptocoinsnews.com/make-backup-genome-using-bitcoin/" },
+                      insidebitcoin: { title: "Inside Bitcoins", link: "http://insidebitcoins.com/news/human-backups-on-the-blockchain/26231" },
+                      bitecoinmagazine: { title: "Bitcoin Magazine", link: "http://bitcoinmagazine.com/18205/genecoin-dna-for-the-blockchain/" },
+                      thestack: { title: "The Stack", link: "http://thestack.com/bitcoin-dna-genecoin-12114" },
+                      cointelegraph: { title: "Coin Telegraph", link: "http://cointelegraph.com/news/112937/genecoin-developers-on-storing-dna-with-bitcoin-and-growing-blockchain-creatures-" },
+                      forexminute: { title: "Forex Minute", link: "http://www.forexminute.com/bitcoin/now-backup-dna-data-using-bitcoin-network-genecoin-48600" },
+                      futurescope: { title: "Future Scope", link: "http://futurescope.co/post/101696220860/genecoin-make-a-backup-of-yourself-using" },
+                      newaesthetic: { title: "New Aesthetic", link: "http://new-aesthetic.tumblr.com/post/101413131391/at-genecoin-we-help-people-upload-their-genome" } };
+
+var bitelabsPress = { time: { title: "Time Magazine", link: "http://newsfeed.time.com/2014/02/28/celebrity-meat-bite-labs-fake/" },
+                      latimes: { title: "The LA Times", link: "http://www.latimes.com/food/dailydish/la-dd-bitelabs-make-salami-celebrity-tissue-samples-20140304-story.html" },
+                      wired: { title: "Wired", link: "http://www.wired.co.uk/news/archive/2014-02/27/celebrity-meat" },
+                      vice: { title: "Vice Motherboard", link: "http://motherboard.vice.com/read/the-guy-who-want-to-sell-you-salami-made-out-of-james-franco-are-100-serious" },
+                      usatoday: { title: "USA Today", link: "http://www.usatoday.com/story/news/nation-now/2014/02/27/celebrity-salami-bite-labs/5861595/" },
+                      huffingtonpost: { title: "Huffington Post", link: "http://www.huffingtonpost.com/2014/03/03/celebrity-salami-bitelabs_n_4890762.html" },
+                      salon: { title: "Salon", link: "http://www.salon.com/2014/02/28/this_company_wants_to_make_in_vitro_meat_from_celebrities_stem_cells/" },
+                      mtv: { title: "MTV News", link: "http://www.mtv.com/news/1723064/bitelabs-eat-celebrity-meat/" },
+                      cosmopolitan: { title: "Cosmopolitan", link: "http://www.cosmopolitan.com/entertainment/celebs/news/a21230/celebrity-meat-company/" },
+                      gawker: { title: "Gawker", link: "http://gawker.com/salami-made-from-kanye-west-is-probably-not-the-future-1532593150" },
+                      newstatesman: { title: "New Statesman", link: "http://www.newstatesman.com/future-proof/2014/02/science-behind-making-your-very-own-james-franco-furter" },
+                      gizmodo: { title: "Gizmodo", link: "http://gizmodo.com/your-next-artisanal-salami-could-be-part-kanye-1532800282?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+gizmodo%2Ffull+%28Gizmodo%29" },
+                      independent: { title: "The Independent", link: "http://www.independent.co.uk/news/people/news/jennifer-lawrence-salami-lab-campaigns-to-make-sausage-meat-out-of-actress-body-tissue-9159832.html" },
+                      hackernews: { title: "Hacker News", link: "https://news.ycombinator.com/item?id=7301353" },
+                      bloomberg: { title: "Bloomberg TV", link: "http://www.bloomberg.com/video/take-a-bite-out-of-an-oscar-winner-FrIlIic8SDO4~f_zBt~Gtw.html" },
+                      slate: { title: "Slate Magazine", link: "http://www.slate.com/blogs/future_tense/2014/02/26/bite_labs_is_probably_a_prank_artisanal_salami_from_celebrity_tissue_samples.html" },
+                      io9: { title: "io9", link: "http://io9.com/in-a-future-of-lab-grown-food-could-you-eat-meat-made-1532525928" },
+                      metro: { title: "link9", link: "http://www.metro.us/news/bitelabs-startup-plans-to-make-celebrity-flavored-meats-with-cell-samples/tmWnbA---49rIrHRPlHn7Q/" },
+                      newharvest: { title: "New Harvest", link: "http://www.new-harvest.org/2014/02/this-company-wants-to-make-in-vitro-meat-from-celebrities-stem-cells-salon/" },
+                      cnet: { title: "CNet", link: "http://www.cnet.com/news/lab-making-salami-out-of-jennifer-lawrence/" },
+                      kotaku: { title: "Kotaku", link: "http://kotaku.com/a-possibly-fake-startup-is-proposing-we-eat-the-rich-1530628936" } };
+
+var mcmassPress= { fox : { title: "Fox News", link: "http://www.foxnews.com/leisure/2014/11/28/businessman-wants-to-build-mcdonalds-in-church/" },
+                   nbcnews: { title: "NBC News", link: "http://www.nbcnews.com/business/business-news/mcmass-project-aims-put-mcdonalds-restaurant-church-n255991" },
+                   chicagotribune : { title: "The Chicago Tribune", link: "http://www.chicagotribune.com/news/nationworld/la-dd-mcmass-mcdonalds-in-church-20141126-story.html" },
+                   latimes : { title: "The LA Times", link: "http://www.latimes.com/food/dailydish/la-dd-mcmass-mcdonalds-in-church-20141126-story.html" },
+                   washingtontimes: { title: "The Washington Times", link: "http://www.washingtontimes.com/news/2014/nov/28/mcdonalds-at-mass-mcmass-pushes-to-build-restauran/" },
+                   animalny: { title: "Animal New York", link: "http://animalnewyork.com/2014/mcmass-indiegogo-campaign-isnt-scam-stupid-hackathon-art-project/" },
+                   businessinsider: { title: "Business Insider", link: "http://www.businessinsider.com/alabama-church-in-buffalo-wild-wings-2014-12" },
+                   theyoungturks: { title: "The Young Turks", link: "https://www.youtube.com/watch?v=A--4GKsVZow" },
+                   msnbc: { title: "MSNBC", link: "https://www.youtube.com/watch?v=TQsmoINBSRk" },
+                   eater: { title: "Eater", link: "http://www.eater.com/2014/11/25/7288723/religious-group-wants-to-open-a-mcdonalds-franchise-inside-a-church" },
+                   vocativ: { title: "Vocativ", link: "http://www.vocativ.com/culture/society/mcmass-project/" },
+                   mirror: { title: "Mirror", link: "http://www.mirror.co.uk/news/weird-news/mcdonalds-open-church-mcmass-attract-4711669" },
+                   reddit: { title: "Reddit", link: "https://www.reddit.com/r/offbeat/comments/2nnxfs/mcmass_wants_to_put_mcdonalds_in_a_church/" },
+                   entrepreneur: { title: "Entrepreneur", link: "http://www.entrepreneur.com/article/240358" },
+                   hngn: { title: "Headlines and Global News", link: "http://www.hngn.com/articles/50789/20141125/mcdonalds-in-church-thats-the-goal-of-one-crowdfunding-campaign.html" },
+                   nbcbusiness: { title: "NBC Business", link: "https://www.youtube.com/watch?v=TQsmoINBSRk" },
+                   christiantoday: { title: "Christian Today", link: "http://www.christiantoday.com/article/christian.group.fudraising.to.open.mcdonalds.in.a.church/43565.html" },
+                   dailymail: { title: "Daily Mail", link: "http://www.dailymail.co.uk/news/article-2853017/Would-like-Happy-Clappy-Meal-Religious-group-unveils-plans-entice-worshippers-opening-McDonald-s-CHURCH.html" },
+                   foxbusiness: { title: "Fox Business", link: "https://www.youtube.com/watch?v=hxt243QYkKI" },
+                   rawstory: { title: "Raw Story", link: "http://www.rawstory.com/rs/2014/11/christian-branding-agency-seeks-1-million-for-mcmass-project-to-put-mcdonalds-in-churches/" },
+                   oregonian: { title: "The Oregonian", link: "http://www.oregonlive.com/today/index.ssf/2014/11/mcdonalds_and_church_make_for.html" },
+                   deseretnews: { title: "Deseret News", link: "http://national.deseretnews.com/article/2846/do-you-want-faith-with-that-new-crowd-funding-campaign-aims-to-bring-mcdonalds-to-church.html" } };
+
+
+$( document ).ready( function() {
+    // expanding text
+    $( ".project" ).each( function() {
+        $( this ).on( 'click', function() {
+            $( this ).off( 'click' );
+            open( $( this ), $( this ).attr( "data-project" ) );
+        });
+    });
+
+    // width to screen width
+    $( ".text" ).width( $( window ).width() - 20 );
+    $( window ).resize( function() {
+        $( ".text" ).width( $( window ).width() - 20 );
+    });
+
+    // sticky cursor image
+
+});
+
+function getPressObject( project ) {
+    switch( project ) {
+        case "bitelabs":
+            return bitelabsPress;
+        case "genecoin":
+            return genecoinPress;
+        case "mcmass":
+            return mcmassPress;
+    }
+};
+
+function open( el, project ) {
+    console.log( project );
+    var pressList = stringifyPress( getPressObject( project ) );
+    var color = el.parent().attr( "data-color" );
+
+    var $press = $( "<span></span>" )
+                 .addClass( "press" )
+                 .attr( "id", project )
+                 .css( "color", "#"+color );
+
+    el.after( $press );
+    openPress( el, $press, pressList, getPressObject( project ) );
+};
+
+function close( el ) { 
+    el.off( 'click' );
+    var press = $( "#"+el.attr( "data-project" ) );
+    delinkifyPress( press );
+};
+
+// recursive list builder
+// args: { el: element of list, 
+//         list: list to open,
+//         bitelabsPress: obj }
+function openPress( projectEl, el, list, pressObject ) {
+    if( list.length > 0 ) {
+        setTimeout( function() {
+            el.append( list[0] );
+            list = list.substring( 1, list.length );
+            openPress( projectEl, el, list, pressObject );
+        }, 15);
+    } else {
+        linkifyPress( projectEl, el, pressObject );
+    }
+};
+
+function closePress( el ) {
+    if( el.html().length > 0 ) {
+        setTimeout( function() {
+            var text = el.html();
+            el = el.html( text.slice(0, text.length-1) );
+            closePress( el );    
+        }, 10);
+    } else {
+        var project = el.attr( "id" );
+        var projectEl = $( ".project[data-project='"+project+"']" );
+
+        el.remove();
+        projectEl.on( 'click', function() { 
+            $( this ).off( 'click' );
+            open( projectEl, project ); 
+        });
+    }
+};
+
+// turns strings in element into links
+function linkifyPress( projectEl, el, pressObject ) {   
+    var text = el.html().trim();
+    var list = text.substring( 1, text.length - 1).split( "," );
+    var counter = 0,
+        listLength = list.length;
+
+    var newText = " [";
+    for( var e in list ) {
+        var key = getKeyByName( list[ e ].trim(), pressObject ),
+            title = pressObject[ key ].title,
+            link = pressObject[ key ].link;
+
+        newText += "<a href='"+link+"' target='_blank'>"+title+"</a>" 
+        if( counter != list.length-1 ) {
+            newText += ", ";
+        }
+        counter++;
+    }
+    newText += "]";
+    el.html( newText );
+
+    // finished! (create new click event)
+    console.log( projectEl.attr( "data-project" ) );
+    projectEl.on( 'click', function() { close( projectEl ); } )
+};
+
+// turns string of links into non-links
+function delinkifyPress( el ) {
+    var text = el.html().trim();
+    var list = text.substring( 1, text.length-1).split( "," );
+    var counter = 0,
+        listLength = list.length;
+
+    var pressList = " [";
+    for( var e in list ) {
+        var pressItem = $( list[ e ].trim() ).html();
+        pressList += pressItem;
+        if( counter != list.length-1 ) {
+            pressList += ", ";
+        }
+        counter++;
+    }
+    pressList += "]";
+    el.html( pressList );
+
+    closePress( el );
+};
+
+function stringifyPress( pressList ) {
+    var press = " [";
+    var pressLength = size( pressList );
+    var counter = 0;
+    for( var key in pressList ) {
+        var pressTitle = pressList[ key ].title;
+
+        var pressString = pressTitle;
+        if( counter != pressLength-1 ) {
+            pressString += ", ";
+        }
+        counter++;
+        press += pressString;
+    }   
+    press += "]";
+    return press;
+};
+
+function size(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
+function getKeyByName( name, pressObject ) {
+    for( var key in pressObject ) {
+        var item = pressObject[key];
+        if( item.title == name ) {
+            return key;
+        }
+    }
+    return null;
+};
